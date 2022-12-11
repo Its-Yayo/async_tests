95b8f5be481f8be0261b029214ce3d1108d5f4d8
a01754574 <a01754574@tec.mx>
2022-12-11 14:08:07 -0600

    Update

--- src/future_async.py
+++ src/future_async.py
@@ -4,6 +4,12 @@ import time
 import asyncio as asy
 from asyncio import futures
 from functools import partial
+from concurrent.futures import ThreadPoolExecutor
+
+
+
+if __name__ == "__main__":
+    asy.run(main())
 
 
 
