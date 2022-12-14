03bc39a42c8e942b851052ca783ca22940956892
a01754574 <a01754574@tec.mx>
2022-12-14 15:10:07 -0600

    Update Tasks

--- src/future_async.py
+++ src/future_async.py
@@ -6,16 +6,31 @@ from asyncio import futures
 from functools import partial
 from concurrent.futures import ThreadPoolExecutor
 
-def handle_event(event):
-    return event
+# Long-run computation
+def some_compute():
+    return 42
 
-async def add_
+# Setting future object
+async def set_future(fut, delay, value, result):
+    await asy.sleep(delay)
+    fut.set_result(value)
 
-
+# Creating future and awaiting a single ThreadPoolExecutor with a single worker thread
 async def main():
     print("Time started at: ", time.strftime('%X'))
 
     loop = asy.get_event_loop()
+    fut = loop.create_future() 
+
+    result = fut.result()
+    
+    while True: 
+        try:    
+            task = loop.create_task(
+                set_future(fut, 2, "..future", result)
+            )
+        except asy.InvalidStateError:
+            continue
 
 
 if __name__ == "__main__":
